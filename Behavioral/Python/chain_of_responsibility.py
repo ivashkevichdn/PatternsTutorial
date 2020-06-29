4548a6b2f81cd8dcb1a569e4104c735f154ea0d5
Ivashkevich Dmitriy <35060908+ivashkevichdn@users.noreply.github.com>
2020-06-29 16:29:36 +0300

    Update chain_of_responsibility.py
    Исправление всего кода реализации примера паттерна "Итератор" на более очевидный пример.

--- Behavioral/Python/chain_of_responsibility.py
+++ Behavioral/Python/chain_of_responsibility.py
@@ -1,138 +1,105 @@
 # coding: utf-8
 
-"""
-Цепочка обязанностей (Chain of Responsibility) - паттерн поведения объектов.
-
-Позволяет избежать привязки отправителя запроса к его получателю,
-давая шанс обработать запрос нескольким объектам. Связывает объекты-получатели в цепочку
-и передает запрос вдоль этой цепочки, пока его не обработают.
-"""
-
-
 from __future__ import annotations
-from abc import ABC, abstractmethod
-from typing import Any, Optional
-
-
-class Handler(ABC):
-    """
-    Интерфейс Обработчика объявляет метод построения цепочки обработчиков. Он
-    также объявляет метод для выполнения запроса.
-    """
-
-    @abstractmethod
-    def set_next(self, handler: Handler) -> Handler:
-        pass
-
-    @abstractmethod
-    def handle(self, request) -> Optional[str]:
-        pass
-
-
-class AbstractHandler(Handler):
-    """
-    Поведение цепочки по умолчанию может быть реализовано внутри базового класса
-    обработчика.
-    """
-
-    _next_handler: Handler = None
-
-    def set_next(self, handler: Handler) -> Handler:
-        self._next_handler = handler
-        # Возврат обработчика отсюда позволит связать обработчики простым
-        # способом, вот так:
-        # monkey.set_next(squirrel).set_next(dog)
-        return handler
-
-    @abstractmethod
-    def handle(self, request: Any) -> str:
-        if self._next_handler:
-            return self._next_handler.handle(request)
-
-        return None
+from collections.abc import Iterable, Iterator
+from typing import Any, List
 
 
 """
-Все Конкретные Обработчики либо обрабатывают запрос, либо передают его
-следующему обработчику в цепочке.
+Для создания итератора в Python есть два абстрактных класса из встроенного
+модуля collections - Iterable, Iterator. Нужно реализовать метод __iter__() в
+итерируемом объекте (списке), а метод __next__() в итераторе.
 """
 
 
-class MonkeyHandler(AbstractHandler):
-    def handle(self, request: Any) -> str:
-        if request == "Banana":
-            return f"Monkey: I'll eat the {request}"
-        else:
-            return super().handle(request)
-
-
-class SquirrelHandler(AbstractHandler):
-    def handle(self, request: Any) -> str:
-        if request == "Nut":
-            return f"Squirrel: I'll eat the {request}"
-        else:
-            return super().handle(request)
-
-
-class DogHandler(AbstractHandler):
-    def handle(self, request: Any) -> str:
-        if request == "MeatBall":
-            return f"Dog: I'll eat the {request}"
-        else:
-            return super().handle(request)
-
-
-def client_code(handler: Handler) -> None:
+class AlphabeticalOrderIterator(Iterator):
     """
-    Обычно клиентский код приспособлен для работы с единственным обработчиком. В
-    большинстве случаев клиенту даже неизвестно, что этот обработчик является
-    частью цепочки.
+    Конкретные Итераторы реализуют различные алгоритмы обхода. Эти классы
+    постоянно хранят текущее положение обхода.
     """
 
-    for food in ["Nut", "Banana", "Cup of coffee"]:
-        print(f"\nClient: Who wants a {food}?")
-        result = handler.handle(food)
-        if result:
-            print(f"  {result}", end="")
-        else:
-            print(f"  {food} was left untouched.", end="")
+    """
+    Атрибут _position хранит текущее положение обхода. У итератора может быть
+    множество других полей для хранения состояния итерации, особенно когда он
+    должен работать с определённым типом коллекции.
+    """
+    _position: int = None
+
+    """
+    Этот атрибут указывает направление обхода.
+    """
+    _reverse: bool = False
+
+    def __init__(self, collection: WordsCollection, reverse: bool = False) -> None:
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        """
+        Метод __next __() должен вернуть следующий элемент в последовательности.
+        При достижении конца коллекции и в последующих вызовах должно вызываться
+        исключение StopIteration.
+        """
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+
+class WordsCollection(Iterable):
+    """
+    Конкретные Коллекции предоставляют один или несколько методов для получения
+    новых экземпляров итератора, совместимых с классом коллекции.
+    """
+
+    def __init__(self, collection: List[Any] = []) -> None:
+        self._collection = collection
+
+    def __iter__(self) -> AlphabeticalOrderIterator:
+        """
+        Метод __iter__() возвращает объект итератора, по умолчанию мы возвращаем
+        итератор с сортировкой по возрастанию.
+        """
+        return AlphabeticalOrderIterator(self._collection)
+
+    def get_reverse_iterator(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection, True)
+
+    def add_item(self, item: Any):
+        self._collection.append(item)
 
 
 if __name__ == "__main__":
-    monkey = MonkeyHandler()
-    squirrel = SquirrelHandler()
-    dog = DogHandler()
+    # Клиентский код может знать или не знать о Конкретном Итераторе или классах
+    # Коллекций, в зависимости от уровня косвенности, который вы хотите
+    # сохранить в своей программе.
+    collection = WordsCollection()
+    collection.add_item("First")
+    collection.add_item("Second")
+    collection.add_item("Third")
 
-    monkey.set_next(squirrel).set_next(dog)
+    print("Straight traversal:")
+    print("\n".join(collection))
+    print("")
 
-    # Клиент должен иметь возможность отправлять запрос любому обработчику, а не
-    # только первому в цепочке.
-    print("Chain: Monkey > Squirrel > Dog")
-    client_code(monkey)
-    print("\n")
-
-    print("Subchain: Squirrel > Dog")
-    client_code(squirrel)
+    print("Reverse traversal:")
+    print("\n".join(collection.get_reverse_iterator()), end="")
 
     # -------------------------------------------------------------------------------
     # Результат выполнения
     # -------------------------------------------------------------------------------
     """
-    Chain: Monkey > Squirrel > Dog
+    Straight traversal:
+    First
+    Second
+    Third
 
-    Client: Who wants a Nut?
-        Squirrel: I'll eat the Nut
-    Client: Who wants a Banana?
-        Monkey: I'll eat the Banana
-    Client: Who wants a Cup of coffee?
-        Cup of coffee was left untouched.
-
-    Subchain: Squirrel > Dog
-
-    Client: Who wants a Nut?
-        Squirrel: I'll eat the Nut
-    Client: Who wants a Banana?
-        Banana was left untouched.
-    Client: Who wants a Cup of coffee?
-        Cup of coffee was left untouched.
+    Reverse traversal:
+    Third
+    Second
+    First
     """
